d961f56e2ef38f8914cc8fdb2a689b671f720544
abhishekmarkad82 <106246692+abhishekmarkad82@users.noreply.github.com>
2022-05-26 17:04:21 +0530

    Delete login.js

--- login.js
+++
@@ -1,8 +0,0 @@
-var xml=new XMLHttpRequest();
-xml.open('get','https://examportal-3p34g81ekn6l3lhij0z.codequotient.in/login');
-xml.onload=function(){
-    console.log(this.responseText);
-}
-//xml.setRequestHeader("Content-type","application/json");
-xml.send();
-//JSON.stringify ({ username:"admin", password:123})
